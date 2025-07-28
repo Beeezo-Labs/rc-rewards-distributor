8646d6fcf7a15f454839be3908313f0c97c3cf51
pastrn <y.pasternak@beeezo.com>
2025-07-28 18:06:03 +0200

    feat: configure docgen

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,17 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@openzeppelin/hardhat-upgrades';
 import '@nomicfoundation/hardhat-toolbox';
+import "solidity-docgen";
 
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
   gasReporter: {
     enabled: false,
+  },
+  docgen: {
+    outputDir: "./docs",
+    pages: "files",
+    exclude: ["mocks", "interfaces"],
   },
 };
 
